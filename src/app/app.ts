de88d42d903a5d44b39fd2ecac81882befcc5ed9
Dupertroll <laqm0103@gmail.com>
2022-12-24 20:51:58 -0500

    Feat(app/):Important things have been added to the page
    The bases of the page were added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,12 @@
 import 'normalize.css';
+import './app.css';
+
+const root = document.querySelector('#app');
+let card: Element | null = document.createElement('div');
+let advice: Element | null = document.createElement('p');
+root?.classList.add('root');
+card.classList.add('card');
+advice.textContent =
+	'When youre at a concert or event, enjoy the moment, enjoy being there. Try leaving your camera in your pocket.';
+card.appendChild(advice);
+root?.appendChild(card);
