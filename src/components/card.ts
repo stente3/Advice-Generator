de49ef1343503ea02f22156ff7d05e8f110dc81a
Stente3 <stentem0103@gmail.com>
2023-01-11 14:17:33 -0500

    Refactor(Card.ts): Changed the way the card and its items are created
    With the aim of making card elements more reusable

--- src/components/card.ts
+++ src/components/card.ts
@@ -3,42 +3,52 @@ import divisorDesktop from '../images/pattern-divider-desktop.svg';
 import divisorMobile from '../images/pattern-divider-mobile.svg';
 
 interface Advice {
-	advice: string;
 	id: number;
+	advice: string;
+}
+// Card variables
+const card = document.createElement('div');
+const cardId = document.createElement('p');
+const cardAdvice = document.createElement('p');
+const cardImage = document.createElement('img');
+
+card.classList.add('card');
+cardId.classList.add('id');
+cardImage.classList.add('image');
+
+// Button
+function button() {
+	let button = document.createElement('button');
+	button.classList.add('button');
+	return button;
 }
 
+// Card component
 async function Card(): Promise<Element> {
-	let service = await apiData();
-	const card = document.createElement('div');
-	childRemover(card);
-	card.classList.add('card');
-	card.innerHTML = `
-    <p class="id">ADVICE #${service.id}</p>
-    <p>"${service.advice}"</p>
-    <img src="${divisorImage()}" class="image">
-    <button class="button"></button>
-`;
+	let { id, advice }: Advice = await apiData();
+
+	cardId.textContent = `ADVICE #${id}`;
+	cardAdvice.textContent = `"${advice}"`;
+	cardImage.src = divisorImage();
+
+	card.appendChild(cardId);
+	card.appendChild(cardAdvice);
+	card.appendChild(cardImage);
+	card.appendChild(button());
+
 	return card;
 }
 
-/* Consulta los datos a la API y retorna los datos que necesitan ser mostrados */
+/* Utilities */
+// Consulta los datos a la API y retorna los datos que necesitan ser mostrados
 async function apiData(): Promise<Advice> {
 	let apiMessage = await api();
-	let { advice, id } = apiMessage.slip;
-	return { advice, id };
+	let { id, advice } = apiMessage.slip;
+	return { id, advice };
 }
-
-/* Utilities */
 // Comprobacion de la resolucion
 function divisorImage() {
 	return screen.width > 375 ? divisorDesktop : divisorMobile;
-}
-
-// Elimina todos los hijos del elemento dado
-function childRemover(parent: Element) {
-	while (parent.firstChild) {
-		parent.removeChild(parent.firstChild);
-	}
 }
 
 export default Card;
