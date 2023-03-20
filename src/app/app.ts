356aa99ce0c90594513220228c6f2b9fdbf84bb6
Stente3 <stentem0103@gmail.com>
2023-03-20 10:23:31 -0500

    Fix(Async function): It was encapsulated inside an asynchronous function
    The line of code that adds the card is now encapsulated in an asynchronous function

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,4 +4,7 @@ import Card from '../components/card';
 /* Creacion de elementos */
 const root = document.querySelector('#app');
 root?.classList.add('root');
-root?.appendChild(await Card());
+append();
+async function append(){
+  root?.appendChild(await Card());
+}
